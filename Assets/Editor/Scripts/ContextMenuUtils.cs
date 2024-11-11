159fa7dd2a5aef273d56b7ea51485a2df9cb6fab
DevFurina <46137287+Furinaxi@users.noreply.github.com>
2024-11-11 16:13:51 +0100

    Added utility script for game designer

---
+++ Assets/Editor/Scripts/ContextMenuUtils.cs
@@ -0,0 +1,50 @@
+#if UNITY_EDITOR
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class ContextMenuUtils
+{
+    private const string PathToFishAreaPrefab = "Assets/Prefabs/Levelelements/FishArea.prefab";
+    private const string PathToSpawnPointPrefab = "Assets/Prefabs/Levelelements/FishSpawnPoint.prefab";
+    private const string PathToHatcheryPrefab = "Assets/Prefabs/Levelelements/SpawningGround.prefab";
+    
+    [MenuItem("GameObject/Level Elements/Fish Area", false, 4)]
+    public static void AddFishArea()
+    {
+        InstantiatePrefabAtPath(PathToFishAreaPrefab, "Fish Area");
+    }
+    
+    [MenuItem("GameObject/Level Elements/Spawn Point", false, 4)]
+    public static void AddSpawnPoint()
+    {
+        InstantiatePrefabAtPath(PathToSpawnPointPrefab, "Spawn Point");
+    }
+    
+    [MenuItem("GameObject/Level Elements/Hatchery", false, 4)]
+    public static void AddHatchery()
+    {
+        InstantiatePrefabAtPath(PathToHatcheryPrefab, "Hatchery");
+    }
+    
+    private static void InstantiatePrefabAtPath(string path, string name)
+    {
+        Object prefab = AssetDatabase.LoadAssetAtPath<Object>(path);
+        
+        if (prefab)
+        {
+            GameObject instance = (GameObject) PrefabUtility.InstantiatePrefab(prefab);
+            instance.name = name;
+            instance.transform.position = Vector3.zero;
+            Selection.activeGameObject = instance;
+        }
+        else
+        {
+            Debug.LogError(name + " prefab not found at path: " + path);
+        }
+    }
+}
+
+#endif
