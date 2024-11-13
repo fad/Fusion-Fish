438144e8c5d5a2d76a43f222a335c43a93ed62c9
LLl-Y-T <nekto118@gmail.com>
2024-11-13 16:46:43 +0100

    Decal update

---
+++ Assets/Scripts/DecalController/DecalController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class DecalController : MonoBehaviour
+{
+    [SerializeField]private DecalProjector[] decals;
+
+    
+}
